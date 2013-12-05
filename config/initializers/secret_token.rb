e21224d922e558e36d235f2d7d842c3f039502f3
Yonathan Ayenew <yonayen@gmail.com>
2013-12-05 14:02:44 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FairdimeApp::Application.config.secret_key_base = 'fe94b8692e1204760a0c18ba58eb397a995466349a431abfb0584748d674a2c1c15ae2fd571333eb767ae2019fb337d61cac4e494a7d476dcae236d9c6642041'
