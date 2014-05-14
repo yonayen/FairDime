3d3b35f116503dc3be6a3fa6681fde1cf04c3fc1
Yonathan Ayenew <yonayen@gmail.com>
2014-05-14 13:30:33 -0700

    Cleaned up pins scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FairdimeApp::Application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140508001938) do
+ActiveRecord::Schema.define(version: 20140513191450) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.string   "string"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
