89a0176797e3c916305c08931a57f61f2fded9af
Yonathan Ayenew <yonayen@gmail.com>
2014-05-17 13:27:47 -0700

    Fix Heroku Pin and S3 error

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"}
+	has_attached_file :image, 
+	:styles => { :medium => "300x300>", :thumb => "100x100>"}
+	:storage => :s3,
+  :bucket => 'OMfairpercent',
+  :s3_credentials => {:access_key_id => ENV['AKIAI4IDCMKQ3FZSWK5Q'], :secret_access_key => ENV['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']}
 end
