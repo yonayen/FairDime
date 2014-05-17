d8e194d54f7687eb7d6081128362860a0f746692
Yonathan Ayenew <yonayen@gmail.com>
2014-05-17 14:09:54 -0700

    Troubleshooting Heroku error prt 2

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,5 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, 
 	:styles => { :medium => "300x300>", :thumb => "100x100>"}
-	:storage => :s3,
-  :bucket => 'OMfairpercent',
-  :s3_credentials => {:access_key_id => ENV['AKIAI4IDCMKQ3FZSWK5Q'], :secret_access_key => ENV['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']}
+	
 end
